8e2eb08efc1341fe1c03e60ee300b09474579f40
piyushkochhar <piyush.kochhar1@gmail.com>
2022-07-25 09:41:12 -0500

    Refactor UserInfo.java

--- src/HW1/UserInfo.java
+++ src/HW1/UserInfo.java
@@ -2,15 +2,25 @@ package HW1;
 
 public class UserInfo {
 
-	private static String firstName;
-	private static String lastName;
-	private static int age;
-	private static String phone;
-	private static String email;
-	private static String country;
-	private static int postalZip;
-
-	public static void printUserInfo() {
+	private String firstName;
+	private String lastName;
+	private int age;
+	private String phone;
+	private String email;
+	private String country;
+	private int postalZip;
+	
+	UserInfo(String firstName, String lastName, int age, String country, int postalZip, String phone, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.country = country;
+		this.postalZip = postalZip;
+		this.phone = phone;
+		this.email = email;
+	}
+	
+	public void printUserInfo() {
 		String result = "Your name is " + firstName + " " + lastName + ". Your age is " + age + ". You reside in "
 				+ country + ", with zip code " + postalZip + ". Your phone number is " + phone
 				+ " and your email id is " + email + ".";
@@ -19,16 +29,10 @@ public class UserInfo {
 	}
 
 	public static void main(String[] args) {
+		
+		UserInfo user = new UserInfo(args[0], args[1], Integer.parseInt(args[2]),args[3], Integer.parseInt(args[2]), args[5], args[6]);
 
-		firstName = args[0];
-		lastName = args[1];
-		age = Integer.parseInt(args[2]);
-		country = args[3];
-		postalZip = Integer.parseInt(args[4]);
-		phone = args[5];
-		email = args[6];
-
-		printUserInfo();
+		user.printUserInfo();
 
 	}
 
